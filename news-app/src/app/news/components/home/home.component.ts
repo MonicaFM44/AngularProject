5b5d57091756710d1bf626b2a27783f7a4f56c27
ignacioPastor <ignacio.pastor@clearpeaks.com>
2018-07-10 08:49:16 +0200

    small details

--- news-app/src/app/news/components/home/home.component.ts
+++ news-app/src/app/news/components/home/home.component.ts
@@ -5,5 +5,5 @@ import { Component } from "@angular/core";
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    pageTitle: string = 'News';
+    readonly pageTitle: string = 'News';
 }
